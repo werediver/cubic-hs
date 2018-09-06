a0dfbfe807a0b268291aefa82e0eb7b0c1249ac8
Raman Fedaseyeu <>
2018-09-06 22:37:47 +0200

    Introduce `numberLiteral` and `json` parsers, and `JSON` data type

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,49 @@
 module Main where
 
 import Control.Applicative
+import Data.Maybe (fromMaybe)
 import Parser
 import Extras
+
+data JSON
+  = JSONString String
+  | JSONNumber Double
+  deriving Show
+
+json :: Parser JSON
+json =
+      JSONString <$> stringLiteral
+  <|> JSONNumber <$> numberLiteral
+
+-- NUM_LIT
+-- Integer numbers only.
+numberLiteral :: Parser Double
+numberLiteral =
+  parse
+  where
+    sign    = let minus = satisfy (== '-') in
+              fromMaybe 1 . fmap (const (-1)) <$> optional minus
+    zero    = satisfy (== '0')
+    nonZero = satisfy $ \c -> c >= '1' && c <= '9'
+    digit   = satisfy $ \c -> c >= '0' && c <= '9'
+    parse   = do
+      sign'   <- sign
+      digits  <-  (:[]) <$> zero
+              <|> (:)   <$> nonZero <*> many digit
+      return $ sign' * read digits
 
 -- STR_LIT = QUOTE STR_LIT_BODY QUOTE
 stringLiteral :: Parser String
 stringLiteral = do
-  _ <- quote
-  body <- stringLiteralBody
-  _ <- quote
-  return body
+  quote *> stringLiteralBody <* quote
   where
     quote = satisfy (== '"')
 
 -- STR_LIT_BODY
+-- Escape sequences are not supported.
 stringLiteralBody :: Parser String
 stringLiteralBody =
-    many $ satisfy isStringLiteralChar
+  many $ satisfy isStringLiteralChar
   where
     isStringLiteralChar :: Char -> Bool
     isStringLiteralChar c = c /= '"' && c /= '\\' && c > '\x1F'
@@ -25,4 +51,4 @@ stringLiteralBody =
 main :: IO ()
 main = do
   s <- getLine
-  print $ parse stringLiteral s
+  print $ parse json s
--- src/Parser.hs
+++ src/Parser.hs
@@ -9,12 +9,8 @@ import Data.Functor ((<&>))
 
 newtype Parser a = Parser { parse :: String -> Maybe (a, String) }
 
-runParser :: Parser a -> String -> Maybe a
-runParser p s =
-  parse p s <&> fst
-
 instance Functor Parser where
-  fmap f p = Parser $ \s -> fmap (\(a, s') -> (f a, s')) $ parse p s
+  fmap f p = Parser $ \s -> parse p s <&> \(a, s') -> (f a, s')
 
 instance Applicative Parser where
   pure a = Parser $ \s -> Just (a, s)
@@ -29,3 +25,7 @@ instance Alternative Parser where
 
 instance Monad Parser where
   (>>=) p f = Parser (\s -> parse p s >>= \(a, s') -> parse (f a) s')
+
+runParser :: Parser a -> String -> Maybe a
+runParser p s =
+  parse p s <&> fst
